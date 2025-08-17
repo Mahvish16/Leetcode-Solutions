eb87b59f9a99a063eed668fb08ce053881aee304
Mahvish16 <mahvish.ruhi@gmail.com>
2025-08-17 18:11:00 +0000

    Add climbing stairs solution

---
+++ by_difficulty/easy/Climbing Stairs/Climbing Stairs.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        a,b =1,1
+        if n==0 or n==1:
+            return 1
+        for i in range(2, n+1):
+            a,b=b,a+b
+        return b
